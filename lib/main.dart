55b06f9118b1006df06846c29034b130f9437f1c
zhoucheng <zhoucheng133@outlook.com>
2025-04-14 21:05:25 +0800

    完成暗色模式自动切换

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,45 @@
+import 'package:aria_remote/variables/get_settings.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
   @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+
+  final GetSettings settings=Get.put(GetSettings());
+
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    final Brightness brightness = MediaQuery.of(context).platformBrightness;
+    settings.autoDarkController(brightness == Brightness.dark);
+
+    return Obx(()=>
+      GetMaterialApp(
+        theme: settings.darkMode.value ? ThemeData.dark().copyWith(
+          // 设置暗色模式的字体默认颜色
+          textTheme: GoogleFonts.notoSansScTextTheme().apply(
+              bodyColor: Colors.white,
+              displayColor: Colors.white, 
+          ),
+          colorScheme: ColorScheme.fromSeed(
+              seedColor: Colors.teal,
+              brightness: Brightness.dark,
+          )
+        ) : ThemeData(
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.teal),
+            textTheme: GoogleFonts.notoSansScTextTheme(),
         ),
-      ),
+      )
     );
   }
 }
---
+++ lib/variables/get_settings.dart
@@ -0,0 +1,15 @@
+import 'package:get/get.dart';
+
+class GetSettings extends GetxController{
+  RxBool darkMode=false.obs;
+  RxBool autoDark=true.obs;
+  void autoDarkController(bool dark){
+    if(autoDark.value){
+      darkMode.value=dark;
+    }
+  }
+
+  RxString rpc="".obs;
+  RxString secret="".obs;
+  
+}
