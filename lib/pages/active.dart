e64ee6646aa8c717e9bb9e2a161c0767d01b2d7b
zhoucheng <zhoucheng133@outlook.com>
2025-04-16 19:26:02 +0800

    添加活跃中的添加按钮

--- lib/pages/active.dart
+++ lib/pages/active.dart
@@ -1,4 +1,6 @@
+import 'package:aria_remote/utils/get_settings.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class Active extends StatefulWidget {
   const Active({super.key});
@@ -8,10 +10,34 @@ class Active extends StatefulWidget {
 }
 
 class _ActiveState extends State<Active> {
+
+  final GetSettings settings=Get.find();
+
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('活跃中'),
+    return Stack(
+      children: [
+        Positioned(
+          right: 20,
+          bottom: 20,
+          child: Obx(()=>
+            FloatingActionButton(
+              shape: const CircleBorder(),
+              backgroundColor: Colors.teal,
+              onPressed: settings.isLogin() ? (){
+            
+              } : null,
+              child: const Icon(
+                Icons.add_rounded,
+                color: Colors.white,
+              ),
+            ),
+          )
+        ),
+        const Center(
+          child: Text('Active'),
+        )
+      ],
     );
   }
 }
