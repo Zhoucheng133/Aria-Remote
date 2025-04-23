f3c5b2e8e6e188cca6eda21d7c0217b5164c80d7
zhoucheng <zhoucheng133@outlook.com>
2025-04-23 21:32:36 +0800

    添加暗色模式设置

--- lib/pages/settings.dart
+++ lib/pages/settings.dart
@@ -1,4 +1,5 @@
 import 'package:aria_remote/pages/about.dart';
+import 'package:aria_remote/utils/get_dialogs.dart';
 import 'package:aria_remote/utils/get_functions.dart';
 import 'package:aria_remote/utils/get_settings.dart';
 import 'package:flutter/material.dart';
@@ -17,6 +18,7 @@ class _SettingsState extends State<Settings> {
 
   final GetSettings settings=Get.find();
   final GetFunctions functions=Get.find();
+  final GetDialogs dialogs=Get.find();
 
   @override
   Widget build(BuildContext context) {
@@ -46,6 +48,62 @@ class _SettingsState extends State<Settings> {
             FTileGroup(
               label: Text('应用设置', style: GoogleFonts.notoSansSc(),),
               children: [
+                FTile(
+                  title: Text('暗色模式', style: GoogleFonts.notoSansSc(),),
+                  subtitle: Text(settings.autoDark.value ? '自动' : settings.darkMode.value ? '开启' : '关闭'),
+                  onPress: (){
+                    dialogs.showOkDialogRaw(
+                      context: context, 
+                      title: '深色模式', 
+                      child: StatefulBuilder(
+                        builder: (BuildContext context, StateSetter setState) {
+                          return Obx(()=>
+                            Column(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                Row(
+                                  children: [
+                                    Expanded(child: Text('跟随系统', style: GoogleFonts.notoSansSc(
+                                      fontSize: 15
+                                    ),)),
+                                    Transform.scale(
+                                      scale: 0.8,
+                                      child: FSwitch(
+                                        value: settings.autoDark.value,
+                                        onChange: (val){
+                                          settings.autoDark.value=val;
+                                          final Brightness brightness = MediaQuery.of(context).platformBrightness;
+                                          settings.autoDarkController(brightness == Brightness.dark);
+                                        },
+                                      ),
+                                    )
+                                  ],
+                                ),
+                                Row(
+                                  children: [
+                                    Expanded(child: Text('启用深色模式', style: GoogleFonts.notoSansSc(
+                                      fontSize: 15
+                                    ),)),
+                                    Transform.scale(
+                                      scale: 0.8,
+                                      child: FSwitch(
+                                        value: settings.darkMode.value,
+                                        onChange: settings.autoDark.value ? null : (val){
+                                          settings.darkMode.value=val;
+                                        },
+                                      ),
+                                    )
+                                  ],
+                                )
+                              ],
+                            ),
+                          );
+                        }
+                      ),
+                      okText: '完成'
+                    );
+                  },
+                ),
                 FTile(
                   title: Text('关于Aria Remote', style: GoogleFonts.notoSansSc(),),
                   subtitle: Text(settings.version.value, style: GoogleFonts.notoSansSc(),),
